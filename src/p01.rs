cac14fde0c3084e7f72ad6e12b70d504323130ee
BeckTob <beckmann@uni-bonn.de>
2025-12-01 08:47:05 +0100

    feat(p20): apply rotations

--- src/p01.rs
+++ src/p01.rs
@@ -4,7 +4,6 @@ pub fn solve(part2: bool) -> String {
     42.to_string()
 }
 
-
 fn load_rotations() -> Vec<i128> {
     std::fs::read_to_string("input_01.txt")
         .expect("could not read file")
@@ -17,21 +16,43 @@ fn parse_rotation(line: &str) -> i128 {
     let rl = line.chars().nth(0).unwrap();
     let direction_map = HashMap::from([('R', 1), ('L', -1)]);
     let sign = direction_map.get(&rl).unwrap_or(&0);
-    let abs = line.trim_start_matches('R').trim_start_matches('L').parse::<i128>().unwrap();
+    let abs = line
+        .trim_start_matches('R')
+        .trim_start_matches('L')
+        .parse::<i128>()
+        .unwrap();
     sign * abs
 }
 
+fn apply_rotations(rotations: Vec<i128>, mut init: i128) -> Vec<i128> {
+    rotations
+        .iter()
+        .map(|&rot| {
+            init += rot;
+            init = init.rem_euclid(100);
+            init
+        })
+        .collect()
+}
 
 #[test]
 fn test_load_rotations() {
     let rotations = load_rotations();
     assert_eq!(rotations.len(), 4036);
     assert_eq!(*rotations.first().unwrap(), -49);
-    assert_eq!(*rotations.last().unwrap(), 39 );
+    assert_eq!(*rotations.last().unwrap(), 39);
 }
 
 #[test]
 fn test_parse_rotation() {
     assert_eq!(parse_rotation("R123"), 123);
     assert_eq!(parse_rotation("L45"), -45);
+}
+
+#[test]
+fn test_apply_rotations() {
+    let state = 50;
+    let rotations = Vec::from([-68, -30, 48]);
+    let sequence = apply_rotations(rotations, state);
+    assert_eq!(sequence, vec![82, 52, 0]);
 }
