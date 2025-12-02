1080d005ee617c66151f22102127996e13c980d1
BeckTob <beckmann@uni-bonn.de>
2025-12-02 08:53:16 +0100

    feat(p02): parse

--- src/p02.rs
+++ src/p02.rs
@@ -6,6 +6,22 @@ pub fn solve(part2: bool) -> String {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
+struct Range {
+    first: u32,
+    last: u32,
+}
+
+fn parse_ranges(line: &str) -> Vec<Range> {
+    let ranges = line.split(',');
+    ranges
+        .map(|s| {
+            let ids: Vec<u32> = s.split('-').map(|s| s.parse().unwrap()).collect();
+            Range { first: *ids.first().unwrap(), last: *ids.last().unwrap() }
+        })
+        .collect()
+}
+
 fn invalid(id: u32) -> bool {
     let n_digits: u32 = id.to_string().len().try_into().unwrap(); // Speedup with log?
 
@@ -29,4 +45,15 @@ fn test_invalid() {
     assert_eq!(invalid(1010), true);
 
     assert_eq!(invalid(1188511885), true);
+}
+
+#[cfg(test)]
+static EXAMPLE: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+#[test]
+fn test_parse() {
+    let parsed_example = parse_ranges(EXAMPLE);
+
+    assert_eq!(*parsed_example.first().unwrap(), Range { first: 11, last: 22 });
+    assert_eq!(*parsed_example.last().unwrap(), Range { first: 2121212118, last: 2121212124 });
 }
