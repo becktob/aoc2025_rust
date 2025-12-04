e71855e7282a0d59c93a9a9f33c779e5b54466d5
BeckTob <beckmann@uni-bonn.de>
2025-12-04 07:15:26 +0100

    feat(p04): accessible()

--- src/p04.rs
+++ src/p04.rs
@@ -8,16 +8,34 @@ pub fn solve(part2: bool) -> String {
         "WIP".to_string()
     }
 }
-
-type Diagram = HashSet<(usize, usize)>;
+type Roll = (usize, usize);
+type Diagram = HashSet<Roll>;
 
 fn parse_diagram(diagram: &str) -> Diagram {
     diagram
         .lines()
         .enumerate()
-        .flat_map(|(y, line)| line.chars().enumerate().map(move |(x,c)| (y,x,c)))
+        .flat_map(|(y, line)| line.chars().enumerate().map(move |(x, c)| (y, x, c)))
         .filter_map(|(y, x, c)| if c == '@' { Some((y, x)) } else { None })
         .collect()
+}
+
+fn accessible(diagram: &Diagram, roll: &Roll) -> bool {
+    let (y, x) = (roll.0 as i32, roll.1 as i32);
+    let neighbors = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]
+    .iter()
+    .filter(|&&delta| diagram.contains(&((y + delta.0) as usize, (x + delta.1) as usize)))
+    .count();
+    neighbors < 4
 }
 
 #[cfg(test)]
@@ -39,4 +57,11 @@ fn test_parse_diagram() {
     assert_eq!(diagram.len(), 71);
     assert!(!diagram.contains(&(0, 0)));
     assert!(diagram.contains(&(1, 1)));
+}
+
+#[test]
+fn test_accessible() {
+    let diagram = parse_diagram(EXAMPLE1);
+    assert!(accessible(&diagram, &(0, 2)));
+    assert!(!accessible(&diagram, &(1, 1)));
 }
