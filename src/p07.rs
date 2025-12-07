d47d83b6487df0e2ad276d400083e6f9f918aea4
BeckTob <beckmann@uni-bonn.de>
2025-12-07 11:06:49 +0100

    feat(p07): pass_row()

--- src/p07.rs
+++ src/p07.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::ops::{Add, Sub};
 
 pub fn solve(part2: bool) -> String {
     let input = std::fs::read_to_string("input_06.txt").expect("could not read file");
@@ -11,9 +12,11 @@ pub fn solve(part2: bool) -> String {
     }
 }
 
+type BeamRow = HashSet<usize>;
+type SplitterRow = HashSet<usize>;
 struct Manifest {
     start: usize,
-    splitters: Vec<HashSet<usize>>,
+    splitters: Vec<SplitterRow>,
 }
 
 fn parse_manifold(input: &str) -> Manifest {
@@ -23,6 +26,20 @@ fn parse_manifold(input: &str) -> Manifest {
         .map(|line| line.match_indices('^').map(|(i, _)| i).collect())
         .collect();
     Manifest { start, splitters }
+}
+
+fn pass_row(beams: &BeamRow, splitters: &SplitterRow) -> BeamRow {
+    beams
+        .iter()
+        .cloned()
+        .flat_map(|beam| {
+            if splitters.contains(&beam) {
+                vec![beam.sub(1), beam.add(1)]
+            } else {
+                vec![beam]
+            }
+        })
+        .collect()
 }
 
 static EXAMPLE: &str = ".......S.......
@@ -47,9 +64,19 @@ static EXAMPLE: &str = ".......S.......
 fn test_parse_manifest() {
     let manifest = parse_manifold(EXAMPLE);
     assert_eq!(manifest.start, 7);
-    assert_eq!(manifest.splitters[2], HashSet::from_iter([7]));
+    assert_eq!(manifest.splitters[2], SplitterRow::from([7]));
     assert_eq!(
         manifest.splitters[14],
-        HashSet::from_iter([1, 3, 5, 7, 9, 13])
+        SplitterRow::from([1, 3, 5, 7, 9, 13])
     );
+}
+
+#[test]
+fn test_pass_row() {
+    let manifest = parse_manifold(EXAMPLE);
+    let start_beam = BeamRow::from([manifest.start]);
+    let beams_after_first_splitter = pass_row(&start_beam, &manifest.splitters[2]);
+    let beams_after_second = pass_row(&beams_after_first_splitter, &manifest.splitters[4]);
+    assert_eq!(beams_after_first_splitter, BeamRow::from([6, 8]));
+    assert_eq!(beams_after_second, BeamRow::from([5, 7, 9]));
 }
