939a0d5d788c2df6224504b596c8b8401cb1ce89
BeckTob <beckmann@uni-bonn.de>
2025-12-07 10:25:18 +0100

    refactor(p07): simplify using builtin

--- src/p07.rs
+++ src/p07.rs
@@ -13,21 +13,16 @@ pub fn solve(part2: bool) -> String {
 
 struct Manifest {
     start: usize,
-    splitters: Vec<HashSet<usize>>
+    splitters: Vec<HashSet<usize>>,
 }
 
-fn parse_manifold(input: &str) ->  Manifest{
+fn parse_manifold(input: &str) -> Manifest {
     let start = input.lines().nth(0).unwrap().find('S').unwrap();
     let splitters = input
         .lines()
-        .map(|line| {
-            line.chars()
-                .enumerate()
-                .filter_map(|(i, c)| if c == '^' { Some(i) } else { None })
-                .collect()
-        })
+        .map(|line| line.match_indices('^').map(|(i, _)| i).collect())
         .collect();
-    Manifest{start, splitters}
+    Manifest { start, splitters }
 }
 
 static EXAMPLE: &str = ".......S.......
@@ -53,5 +48,8 @@ fn test_parse_manifest() {
     let manifest = parse_manifold(EXAMPLE);
     assert_eq!(manifest.start, 7);
     assert_eq!(manifest.splitters[2], HashSet::from_iter([7]));
-    assert_eq!(manifest.splitters[14], HashSet::from_iter([1,3,5,7,9,13]));
+    assert_eq!(
+        manifest.splitters[14],
+        HashSet::from_iter([1, 3, 5, 7, 9, 13])
+    );
 }
