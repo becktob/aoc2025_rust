0eb3324dc58108d31a5e3b053b83f872e3112c70
BeckTob <beckmann@uni-bonn.de>
2025-12-07 11:14:39 +0100

    refactor(p06): Manifold, not Manifest

--- src/p07.rs
+++ src/p07.rs
@@ -14,18 +14,18 @@ pub fn solve(part2: bool) -> String {
 
 type BeamRow = HashSet<usize>;
 type SplitterRow = HashSet<usize>;
-struct Manifest {
+struct Manifold {
     start: usize,
     splitters: Vec<SplitterRow>,
 }
 
-fn parse_manifold(input: &str) -> Manifest {
+fn parse_manifold(input: &str) -> Manifold {
     let start = input.lines().nth(0).unwrap().find('S').unwrap();
     let splitters = input
         .lines()
         .map(|line| line.match_indices('^').map(|(i, _)| i).collect())
         .collect();
-    Manifest { start, splitters }
+    Manifold { start, splitters }
 }
 
 fn pass_row(beams: &BeamRow, splitters: &SplitterRow) -> BeamRow {
@@ -69,32 +69,32 @@ static EXAMPLE: &str = ".......S.......
 ";
 
 #[test]
-fn test_parse_manifest() {
-    let manifest = parse_manifold(EXAMPLE);
-    assert_eq!(manifest.start, 7);
-    assert_eq!(manifest.splitters[2], SplitterRow::from([7]));
+fn test_parse_manifold() {
+    let manifold = parse_manifold(EXAMPLE);
+    assert_eq!(manifold.start, 7);
+    assert_eq!(manifold.splitters[2], SplitterRow::from([7]));
     assert_eq!(
-        manifest.splitters[14],
+        manifold.splitters[14],
         SplitterRow::from([1, 3, 5, 7, 9, 13])
     );
 }
 
 #[test]
 fn test_pass_row() {
-    let manifest = parse_manifold(EXAMPLE);
-    let start_beam = BeamRow::from([manifest.start]);
-    let beams_after_first_splitter = pass_row(&start_beam, &manifest.splitters[2]);
-    let beams_after_second = pass_row(&beams_after_first_splitter, &manifest.splitters[4]);
+    let manifold = parse_manifold(EXAMPLE);
+    let start_beam = BeamRow::from([manifold.start]);
+    let beams_after_first_splitter = pass_row(&start_beam, &manifold.splitters[2]);
+    let beams_after_second = pass_row(&beams_after_first_splitter, &manifold.splitters[4]);
     assert_eq!(beams_after_first_splitter, BeamRow::from([6, 8]));
     assert_eq!(beams_after_second, BeamRow::from([5, 7, 9]));
 }
 
 #[test]
 fn test_pass_row_count_splits() {
-    let manifest = parse_manifold(EXAMPLE);
-    let beam = BeamRow::from([manifest.start]);
-    let (beam, splits_first) = pass_row_count_splits(&beam, &manifest.splitters[2]);
-    let (_, splits_second) = pass_row_count_splits(&beam, &manifest.splitters[4]);
+    let manifold = parse_manifold(EXAMPLE);
+    let beam = BeamRow::from([manifold.start]);
+    let (beam, splits_first) = pass_row_count_splits(&beam, &manifold.splitters[2]);
+    let (_, splits_second) = pass_row_count_splits(&beam, &manifold.splitters[4]);
     assert_eq!(splits_first, 1);
     assert_eq!(splits_second, 2);
 }
