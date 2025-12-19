d63030b895628f8b2df7f3b0c935df16cc058972
BeckTob <beckmann@uni-bonn.de>
2025-12-19 08:29:38 +0100

    feat(p12): count trivially impossible regions, too.

--- src/p12_explore.rs
+++ src/p12_explore.rs
@@ -1,4 +1,4 @@
-use crate::p12_grid::{Region, parse};
+use crate::p12_grid::{PresentShape, Region, parse};
 
 #[test]
 fn trivially_possible() {
@@ -9,10 +9,42 @@ fn trivially_possible() {
     assert_eq!(trivial, 526)
 }
 
+#[test]
+fn trivially_impossible() {
+    let input = std::fs::read_to_string("input_12.txt").expect("could not read file");
+    let (presents, regions) = parse(&input);
+    let impossible = regions
+        .iter()
+        .filter(|r| cant_even_fit_tiles(r, &presents))
+        .count();
+    assert_eq!(regions.len(), 1000);
+    assert_eq!(impossible, 474)
+}
+
 fn could_fill_blockwise(region: &&Region) -> bool {
     let present_rows = region.height.div_euclid(3);
     let present_cols = region.width.div_euclid(3);
     let trivially_fittable = present_cols * present_rows;
     let total_presents = region.presets_needed.iter().sum::<usize>();
     total_presents <= trivially_fittable
+}
+
+fn cant_even_fit_tiles(region: &&Region, presents: &Vec<PresentShape>) -> bool {
+    let present_sizes = presents
+        .iter()
+        .map(|p| {
+            p.iter()
+                .map(|r| r.iter().filter(|&&p| p).count())
+                .sum::<usize>()
+        })
+        .collect::<Vec<_>>();
+
+    let region_tiles = region.width * region.height;
+    let present_tiles = region
+        .presets_needed
+        .iter()
+        .zip(present_sizes.iter())
+        .map(|(num, size)| num * size)
+        .sum::<usize>();
+    present_tiles > region_tiles
 }
