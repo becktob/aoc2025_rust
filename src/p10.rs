b1dc4e2a3883c6e22e11841e1ad556f490391d8e
BeckTob <beckmann@uni-bonn.de>
2026-01-04 22:02:51 +0100

    feat(p10): configure_joltage() by configuring parity and recursing on half of even machine

--- src/p10.rs
+++ src/p10.rs
@@ -26,10 +26,76 @@ fn solve_1(input: &str) -> usize {
 pub(crate) struct Machine {
     goal: Vec<bool>,
     pub(crate) buttons: Vec<Vec<usize>>,
-    pub(crate) joltage: Vec<i32>,
+    pub(crate) joltage: Vec<u32>,
 }
 
 pub(crate) type ButtonPresses = Vec<usize>; // len == buttons.len; How often is button[i] pushed?
+
+fn configure_joltage(machine: Machine) -> Vec<ButtonPresses> {
+    // TODO: how to test recursion, i.e. "this call leads to correct recurring call"?
+    if machine.joltage.iter().all(|&j| j == 0) {
+        let no_presses = iter::repeat_n(0usize, machine.buttons.len()).collect();
+        return vec![no_presses];
+    }
+
+    let parity_goal = are_odd(&machine.joltage);
+
+    let parity_machine = Machine {
+        goal: parity_goal,
+        buttons: machine.buttons.clone(),
+        joltage: machine.joltage.clone(),
+    };
+
+    let parity_configurations = goal_configurations(&parity_machine);
+    let parity_configurations = parity_configurations
+        .iter()
+        .filter(|&presses| {
+            result_of_presses(presses, &machine) // todo: compute result_of_presses() only once
+                .iter()
+                .zip(machine.joltage.iter())
+                .all(|(r, j)| r <= j)
+        })
+        .collect::<Vec<_>>();
+
+    parity_configurations
+        .iter()
+        .flat_map(|parity_configuration| {
+            // IDEA: subtract parity, recurse on "halved" machine
+            let pressed_joltages = result_of_presses(parity_configuration, &machine);
+            let even_machine = Machine {
+                goal: iter::repeat_n(false, machine.goal.len()).collect(),
+                buttons: machine.buttons.clone(),
+                joltage: machine
+                    .joltage
+                    .iter()
+                    .zip(pressed_joltages.iter())
+                    .map(|(j, p)| j - p)
+                    .collect(),
+            };
+            // TODO: don't need entire machine, only joltage changes
+            let half_machine = Machine {
+                goal: even_machine.goal,
+                buttons: even_machine.buttons,
+                joltage: even_machine.joltage.iter().map(|&j| j / 2).collect(),
+            };
+            let half_machine_solutions = configure_joltage(half_machine);
+            let even_machine_solutions = half_machine_solutions
+                .into_iter()
+                .map(|ps| ps.into_iter().map(|p| p * 2).collect::<ButtonPresses>())
+                .collect::<Vec<_>>();
+
+            even_machine_solutions
+                .iter()
+                .map(move |even| {
+                    even.iter()
+                        .zip(parity_configuration.iter())
+                        .map(|(e, o)| e + o)
+                        .collect::<ButtonPresses>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
 
 fn shortest_goal_configuration(machine: &Machine) -> ButtonPresses {
     goal_configurations(machine)
@@ -42,11 +108,11 @@ fn goal_configurations(machine: &Machine) -> Vec<ButtonPresses> {
     // optimal part 1 solution has 0 or 1 presses per button: 2 presses cancel out
     all_selections(machine.buttons.len())
         .into_iter()
-        .filter(|presses| are_odd(result_of_presses(presses, machine)) == machine.goal)
+        .filter(|presses| are_odd(&result_of_presses(presses, machine)) == machine.goal)
         .collect()
 }
 
-fn are_odd(state: Vec<u32>) -> Vec<bool> {
+fn are_odd(state: &Vec<u32>) -> Vec<bool> {
     state.iter().map(|n| n % 2 == 1).collect::<Vec<_>>()
 }
 
@@ -145,9 +211,50 @@ fn test_all_selections() {
 fn test_result_of_presses() {
     let machine = &parse_machines(EXAMPLE)[0];
     let state = result_of_presses(&vec![0, 1, 0, 1, 0, 2], machine);
-    assert_eq!(are_odd(state), machine.goal);
+    assert_eq!(are_odd(&state), machine.goal);
     let solution_state = result_of_presses(&vec![0, 0, 0, 0, 1, 1], machine);
     assert_eq!(are_odd(&solution_state), machine.goal);
+}
+
+#[test]
+fn test_configure_joltage_single_press() {
+    let machine = Machine {
+        goal: vec![false],
+        buttons: vec![vec![0]],
+        joltage: vec![1],
+    };
+    let solutions = configure_joltage(machine);
+    assert_eq!(solutions, vec![vec![1]]);
+}
+
+#[test]
+fn test_configure_joltage_two_presses() {
+    let machine = Machine {
+        goal: vec![false],
+        buttons: vec![vec![0]],
+        joltage: vec![2],
+    };
+    let solutions = configure_joltage(machine);
+    assert_eq!(solutions, vec![vec![2]]);
+}
+
+#[test]
+fn test_configure_joltage_two_buttons_two_presses() {
+    let machine = Machine {
+        goal: vec![false, false],
+        buttons: vec![vec![0, 1], vec![0]],
+        joltage: vec![2, 1],
+    };
+    let solutions = configure_joltage(machine);
+    assert_eq!(solutions, vec![vec![1, 1]]);
+}
+
+#[test]
+fn test_configure_joltage_example_0() {
+    let machines = parse_machines(EXAMPLE);
+    let solutions = configure_joltage(machines[0].clone());
+    let known_solution = vec![1, 3, 0, 3, 1, 2];
+    assert!(solutions.contains(&known_solution));
 }
 
 #[test]
